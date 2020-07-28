1bdc3703393e42728579c544c0c2ce9fbeacd783
marsO11O <54684855+marsO11O@users.noreply.github.com>
2020-07-28 16:03:55 +0800

    Add files via upload

---
+++ cifar10.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 10:27:40 2020
+
+@author: ml
+"""
+
+from keras.datasets import cifar10
+from keras.utils import np_utils
+from keras.models import Sequential
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D,MaxPooling2D,Dropout,Flatten,Dense
+
+
+def show_image(image):
+    fig = plt.gcf()
+    fig.set_size_inches(2, 2)
+    plt.imshow(image, cmap='binary')
+    plt.show()
+    
+ 
+def show_images_labels_predictions(images,labels,predictions,start_id,num=10):
+    plt.gcf().set_size_inches(12, 14)
+    if num>25: num=25 
+    idxs = np.random.randint(0, len(test_feature), size=25)
+    for i in range(num):
+    
+        ax=plt.subplot(5,5,i+1)
+        ax.imshow(images[idxs[i]], cmap='binary')
+        
+        # 有 AI 預測結果資料, 才在標題顯示預測結果
+        if( len(predictions) > 0 ) :
+            title = 'ai = ' + str(predictions[idxs[i]])
+            # 預測正確顯示(o), 錯誤顯示(x)
+            title += (' (o)' if predictions[idxs[i]]==labels[idxs[i]] else ' (x)') 
+            title += '\nlabel = ' + str(labels[idxs[i]])
+        # 沒有 AI 預測結果資料, 只在標題顯示真實數值
+        else :
+            title = 'label = ' + str(labels[idxs[i]])
+            
+        # X, Y 軸不顯示刻度    
+        ax.set_title(title,fontsize=12) 
+        ax.set_xticks([]);ax.set_yticks([])        
+        start_id+=1 
+    plt.show()
+
+
+
+(train_feature, train_label), (test_feature, test_label) = cifar10.load_data()
+print(train_feature.shape, train_label.shape)
+
+
+train_feature_normalize = train_feature/255
+test_feature_normalize = test_feature/255
+
+train_label_onehot = np_utils.to_categorical(train_label, num_classes=10)
+test_label_onehot = np_utils.to_categorical(test_label, num_classes=10)
+
+
+model = Sequential()
+
+model.add(Conv2D(filters=32,
+                 kernel_size=(3, 3),
+                 padding='same',
+                 input_shape = (32,32,3),
+                 activation='relu'))
+model.add(Conv2D(32, (3, 3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+# conv block 2
+model.add(Conv2D(64, (3, 3), padding='same', activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu") )
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+# dense block
+model.add(Flatten())
+model.add(Dense(units=512, activation="relu") )
+model.add(Dropout(0.5))
+model.add(Dense(units=10,activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam', metrics=['accuracy'])
+
+try:
+    model.load_weights("Cifar10_CNN_model_2.weight")
+    print("載入模型參數成功，繼續訓練模型!")
+except :    
+    print("載入模型參數失敗，開始訓練一個新模型!")
+
+train_history = model.fit(x=train_feature_normalize,
+                          y=train_label_onehot,validation_split=0.2,
+                          epochs=20, batch_size=128)
+
+scores = model.evaluate(test_feature_normalize, test_label_onehot, verbose=1)
+print('score : '+scores)
+
+prediction = model.predict_classes(test_feature_normalize)
+show_images_labels_predictions(test_feature,test_label,prediction,0,20)
+
+model.save('Cifar10_CNN_model_2.h5')     #將模型儲存至 HDF5檔案中
+print("\Cifar10_CNN_model_2.h5 模型儲存完畢!")
+model.save_weights("Cifar10_CNN_model_2.weight") # 將參數儲存不含模型
+print("模型參數儲存完畢!")
+
+del model
